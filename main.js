b08f8e6b019da12ecc6317b84696ac1749fdd417
Javier Urrutia <javier.urrutia@mindlabs.cl>
2023-05-06 20:33:35 -0400

    feat: tiling processing

--- main.js
+++ main.js
@@ -52,34 +52,130 @@ function imageDataToTensor(data, dims) {
   return inputTensor;
 }
 
+async function tileProc(inputTensor){
+  const sessionOption1 = { executionProviders: ['webgl'], logSeverityLevel: 0 };
+  const session1 = await ort.InferenceSession.create('./esrgan-small-pre.onnx', sessionOption1);
+  const sessionOption2 = { executionProviders: ['wasm'], logSeverityLevel: 0 };
+  const session2 = await ort.InferenceSession.create('./esrgan-small-end.onnx', sessionOption2);
+
+  const inputDims = inputTensor.dims;
+  const imageW = inputDims[3];
+  const imageH = inputDims[2];
+
+  const rOffset = 0;
+  const gOffset = imageW*imageH;
+  const bOffset = imageW*imageH*2;
+
+  const outputDims = [inputDims[0], inputDims[1], inputDims[2]*4, inputDims[3]*4];
+  const outputTensor = new ort.Tensor("float32", new Float32Array(outputDims[0]*outputDims[1]*outputDims[2]*outputDims[3]), outputDims);
+
+  const outImageW = outputDims[3];
+  const outImageH = outputDims[2];
+  const outROffset = 0;
+  const outGOffset = outImageW*outImageH;
+  const outBOffset = outImageW*outImageH*2;
+
+  const tileSize = 256;
+  const tilesx = Math.ceil( inputDims[3] / tileSize );
+  const tilesy = Math.ceil( inputDims[2] / tileSize );
+
+  const data = inputTensor.data;
+
+  console.log(inputTensor);
+
+  for (let i = 0; i < tilesx; i++) {
+    for (let j = 0; j < tilesy; j++) {
+      const tileW = Math.min(tileSize, imageW - i * tileSize);
+      const tileH = Math.min(tileSize, imageH - j * tileSize);
+      console.log("tileW: " + tileW + " tileH: " + tileH);
+      const tileROffset = 0;
+      const tileGOffset = tileSize*tileSize;
+      const tileBOffset = tileSize*tileSize*2;
+
+      const tileData = new Float32Array(tileSize*tileSize*3);
+      for (let xp = 0; xp < tileSize; xp++) {
+        for (let yp = 0; yp < tileSize; yp++) {
+          const x = xp < tileW ? xp : tileW - 1;
+          const y = yp < tileH ? yp : tileH - 1;
+          const idx = (i * tileSize + x) + (j * tileSize + y) * imageW;
+          tileData[x + y * tileSize + tileROffset] = data[idx + rOffset];
+          tileData[x + y * tileSize + tileGOffset] = data[idx + gOffset];
+          tileData[x + y * tileSize + tileBOffset] = data[idx + bOffset];
+        }
+      }
+
+      const tile = new ort.Tensor("float32", tileData, [1, 3, tileSize, tileSize]);
+      console.log(tile);
+      const resultspre = await session1.run({input: tile});
+      console.log("pre dims:" + resultspre.output.dims);
+      const feed2 = { input: tile, input_pre: resultspre.output };
+      const results = await session2.run(feed2);
+      console.log("proc tile dims:" + results.output.dims);
+
+      const outTileW = tileW*4;
+      const outTileH = tileH*4;
+      const outTileSize = tileSize*4;
+
+      const outTileROffset = 0;
+      const outTileGOffset = outTileSize*outTileSize;
+      const outTileBOffset = outTileSize*outTileSize*2;
+
+      // add tile to output
+      for (let x = 0; x < outTileW; x++) {
+        for (let y = 0; y < outTileH; y++) {
+          const idx = (i * outTileSize + x) + (j * outTileSize + y) * outImageW ;
+          outputTensor.data[idx + outROffset] = results.output.data[x + y * outTileSize + outTileROffset];
+          outputTensor.data[idx + outGOffset] = results.output.data[x + y * outTileSize + outTileGOffset];
+          outputTensor.data[idx + outBOffset] = results.output.data[x + y * outTileSize + outTileBOffset];
+        }
+      }
+      // for (let x = 0; x < tileW; x++) {
+      //   for (let y = 0; y < tileH; y++) {
+      //     const idx = (i * tileSize + x) + (j * tileSize + y) * outImageW ;
+      //     outputTensor.data[idx + outROffset] = tile.data[x + y * tileW + tileROffset];
+      //     outputTensor.data[idx + outGOffset] = tile.data[x + y * tileW + tileGOffset];
+      //     outputTensor.data[idx + outBOffset] = tile.data[x + y * tileW + tileBOffset];
+      //   }
+      // }
+    }
+  }
+  console.log("output dims:" + outputTensor.dims);
+  console.log(outputTensor);
+  return outputTensor;
+
+}
+
 async function run(inputTensor) {
   try {
     const start = Date.now();
 
-    const sessionOption1 = { executionProviders: ['webgl'], logSeverityLevel: 0 };
-    const session1 = await ort.InferenceSession.create('./esrgan-small-pre.onnx', sessionOption1);
-    const sessionOption2 = { executionProviders: ['wasm'], logSeverityLevel: 0 };
-    const session2 = await ort.InferenceSession.create('./esrgan-small-end.onnx', sessionOption2);
+    // const sessionOption1 = { executionProviders: ['webgl'], logSeverityLevel: 0 };
+    // const session1 = await ort.InferenceSession.create('./esrgan-small-pre.onnx', sessionOption1);
+    // const sessionOption2 = { executionProviders: ['wasm'], logSeverityLevel: 0 };
+    // const session2 = await ort.InferenceSession.create('./esrgan-small-end.onnx', sessionOption2);
 
-    // prepare feeds. use model input names as keys.
-    //const feeds = { input: new ort.Tensor('float32', inputData, dims) };
-    const feeds = { input: inputTensor };
+    // // prepare feeds. use model input names as keys.
+    // //const feeds = { input: new ort.Tensor('float32', inputData, dims) };
+    // const feeds = { input: inputTensor };
 
-    // feed inputs and run
-    const resultspre = await session1.run(feeds);
-    const end1 = Date.now();
-    console.log(`Execution time pre: ${end1 - start} ms`);
-    const feed2 = { input: inputTensor, input_pre: resultspre.output };
-    const results = await session2.run(feed2);
-    const end2 = Date.now();
-    console.log(`Execution time end: ${end2 - end1} ms`);
+    // // feed inputs and run
+    // const resultspre = await session1.run(feeds);
+    // const end1 = Date.now();
+    // console.log(`Execution time pre: ${end1 - start} ms`);
+    // const feed2 = { input: inputTensor, input_pre: resultspre.output };
+    // const results = await session2.run(feed2);
+    // const end2 = Date.now();
+    // console.log(`Execution time end: ${end2 - end1} ms`);
+
+    const result = await tileProc(inputTensor);
+    console.log("output dims:" + result.dims);
 
     //display result
     const canvas = document.createElement("canvas");
     const context = canvas.getContext('2d');
-    canvas.height = results.output.dims[2];
-    canvas.width = results.output.dims[3];
-    const img = results.output.toImageData({ format: 'RGB', tensorLayout: 'NCHW'});
+    canvas.height = result.dims[2];
+    canvas.width = result.dims[3];
+    const img = result.toImageData({ format: 'RGB', tensorLayout: 'NCHW'});
     context.putImageData(img, 0, 0);
     document.getElementById("canvas-image").src = canvas.toDataURL();
 
